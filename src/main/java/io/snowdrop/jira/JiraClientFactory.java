b074cf944cd70599eadca7248f72932041ad8fa9
Ioannis Canellos <iocanel@gmail.com>
2020-04-16 10:20:07 +0300

    feat: Introduce setup for JIRA.

---
+++ src/main/java/io/snowdrop/jira/JiraClientFactory.java
@@ -0,0 +1,31 @@
+
+package io.snowdrop.jira;
+
+import java.net.URI;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Produces;
+
+import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.internal.async.AsynchronousJiraRestClientFactory;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+public class JiraClientFactory {
+
+  @ConfigProperty(name="jira.url")
+  String url;
+
+  @ConfigProperty(name="jira.username")
+  String username;
+
+  @ConfigProperty(name="jira.password")
+  String password;
+
+  @Produces
+  public JiraRestClient createClient() {
+    AsynchronousJiraRestClientFactory factory = new AsynchronousJiraRestClientFactory();
+    return factory.createWithBasicHttpAuthentication(URI.create(url), username, password);
+  }
+}
