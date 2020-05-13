6ff80950f91634a6d5df2112ce26a7b765735ce3
A. Costa <ajc101@trikorasolutions.com>
2020-05-13 13:59:31 +0200

    creates/updates 2 guithub issues regarding de weekkly development report and the deve tasks report

--- src/main/java/io/snowdrop/reporting/weekreport/WeekReportEndpoint.java
+++ src/main/java/io/snowdrop/reporting/weekreport/WeekReportEndpoint.java
@@ -1,8 +1,11 @@
 package io.snowdrop.reporting.weekreport;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -15,6 +18,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
+import com.google.common.collect.ImmutableMap;
+
+import org.eclipse.egit.github.core.Label;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.IssueService;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,6 +43,7 @@ public class WeekReportEndpoint {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WeekReportEndpoint.class);
     private static final SimpleDateFormat DF = new SimpleDateFormat("dd/MM/yyyy");
+    private static final SimpleDateFormat WEEK_YEAR_FORMAT = new SimpleDateFormat("w");
 
     @ConfigProperty(name = "github.users")
     Set<String> users;
@@ -44,6 +53,9 @@ public class WeekReportEndpoint {
 
     //  @Inject
     //  Docs docs;
+
+    @Inject
+    GitHubClient client;
 
     @Inject
     GithubReportingService service;
@@ -65,19 +77,21 @@ public class WeekReportEndpoint {
                             userIssues.getOrDefault(r, Collections.emptySet()),
                             userPullRequests.getOrDefault(r, Collections.emptySet())));
         }).collect(Collectors.toSet());
-
-        //      List<Request> prs = createRequests(startTime, endTime, workItems);
-        //      BatchUpdateDocumentRequest body = new BatchUpdateDocumentRequest().setRequests(prs);
-        //      BatchUpdateDocumentResponse response = docs.documents().batchUpdate(reportDocumentId, body).execute();
-        //    } catch (IOException e) {
-        //      throw BotException.launderThrowable(e);
-        //    }
     }
 
     @GET
     @Path("/generate")
     @Produces(MediaType.TEXT_PLAIN)
     public String createReport(@QueryParam("startTime") String startTimeString, @QueryParam("endTime") String endTimeString) {
+        String mdText;
+        String weekNumber;
+        String issueTitle;
+        List<Label> lstLabels = new LinkedList() {{
+            new Label().setName("report");
+        }};
+        org.eclipse.egit.github.core.Issue issue;
+        IssueService issueService = new IssueService(client);
+        List<org.eclipse.egit.github.core.Issue> lstIssue;
         try {
             Date startTime = startTimeString != null ? DF.parse(startTimeString)
                     : Date.from(service.getPullRequestCollector().getStartTime().toInstant());
@@ -85,14 +99,49 @@ public class WeekReportEndpoint {
                     : Date.from(service.getPullRequestCollector().getEndTime().toInstant());
             LOGGER.info("Time: " + startTimeString + "," + endTimeString);
             LOGGER.info("Time: " + startTime + "," + endTime);
+            weekNumber = WEEK_YEAR_FORMAT.format(endTime);
+            LOGGER.info("weekNumber: " + weekNumber);
             populate(startTime, endTime);
-            String mdText = DevelopmentReportImpl.build(startTime, endTime, users).buildWeeklyReport(Repository.findAll().list());
-            LOGGER.info(mdText);
             mdText = WeeklyDevelopmentReportImpl.build(startTime, endTime, users).buildWeeklyReport(Repository.findAll().list());
             LOGGER.info(mdText);
+            updateGithubIssue(mdText, "week_" + weekNumber, lstLabels, issueService, "snowdrop", "snowdrop-team");
+            mdText = DevelopmentReportImpl.build(startTime, endTime, users).buildWeeklyReport(Repository.findAll().list());
+            LOGGER.info(mdText);
+            updateGithubIssue(mdText, "dev_week_" + weekNumber, lstLabels, issueService, "snowdrop", "snowdrop-team");
             return mdText;
         } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
             throw BotException.launderThrowable(e);
+        }
+    }
+
+    private void updateGithubIssue(
+            final String pMdText,
+            final String pIssueTitle,
+            final List<Label> pLstLabels,
+            final IssueService pIssueService,
+            final String pstrUser,
+            final String pstrRepo)
+            throws IOException {
+        org.eclipse.egit.github.core.Issue issue;
+        List<org.eclipse.egit.github.core.Issue> lstIssue = pIssueService.getIssues(pstrUser, pstrRepo,
+                ImmutableMap.of(IssueService.FILTER_LABELS, "report", IssueService.FIELD_FILTER, "in:title:" + pIssueTitle));
+        if (lstIssue.size() > 0) {
+            issue = lstIssue.get(0);
+            LOGGER.info("issue: " + issue);
+            LOGGER.info("title: " + issue.getTitle());
+            LOGGER.info("getUrl: " + issue.getUrl());
+            LOGGER.info("getNumber: " + issue.getNumber());
+            LOGGER.info("getMilestone: " + issue.getMilestone());
+            issue.setLabels(pLstLabels);
+            issue.setBody(pMdText);
+            pIssueService.editIssue(pstrUser, pstrRepo, issue);
+        } else {
+            issue = new org.eclipse.egit.github.core.Issue();
+            issue.setTitle(pIssueTitle);
+            issue.setLabels(pLstLabels);
+            issue.setBody(pMdText);
+            pIssueService.createIssue("snowdrop", "snowdrop-team", issue);
         }
     }
 
