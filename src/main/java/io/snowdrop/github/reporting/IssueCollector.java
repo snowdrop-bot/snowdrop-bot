fa86e8cd3b67c30f4eec9fa55bbbc60898270137
A. Costa <ajc101@trikorasolutions.com>
2020-09-17 12:52:10 +0200

    fix: the query dates weren't being updated so the bot always queried on the same dates

--- src/main/java/io/snowdrop/github/reporting/IssueCollector.java
+++ src/main/java/io/snowdrop/github/reporting/IssueCollector.java
@@ -6,7 +6,6 @@ import java.time.DayOfWeek;
 import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
